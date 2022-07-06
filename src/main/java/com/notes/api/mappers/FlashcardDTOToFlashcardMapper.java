7ef7f6eaabc2a940eb8714f48279e613a016c02b
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-07-06 23:17:11 +0300

    Add mapper to map flashcard blocks and tests

---
+++ src/main/java/com/notes/api/mappers/FlashcardDTOToFlashcardMapper.java
@@ -0,0 +1,31 @@
+package com.notes.api.mappers;
+
+import com.notes.api.dto.FlashcardDTO;
+import com.notes.api.entities.Flashcard;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public abstract class FlashcardDTOToFlashcardMapper {
+
+    public Flashcard toFlashcard(FlashcardDTO flashcardDTO) {
+        Flashcard flashcard = new Flashcard();
+        flashcard.setId(flashcardDTO.getId());
+        flashcard.setAnswer(flashcardDTO.getAnswer());
+        flashcard.setQuestion(flashcardDTO.getQuestion());
+        return flashcard;
+    }
+
+    public abstract List<Flashcard> toFlashcards(List<FlashcardDTO> flashcardDTOS);
+
+    public FlashcardDTO toFlashcard(Flashcard flashcard) {
+        FlashcardDTO flashcardDTO = new FlashcardDTO();
+        flashcardDTO.setId(flashcard.getId());
+        flashcardDTO.setQuestion(flashcard.getQuestion());
+        flashcardDTO.setAnswer(flashcard.getAnswer());
+        return flashcardDTO;
+    }
+
+    public abstract List<FlashcardDTO> toFlashcardDTOs(List<Flashcard> flashcards);
+}
