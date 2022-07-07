27a5db4da3681e2f8e89f7cba40ffab688352f6b
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-07-08 00:08:44 +0300

    Remove id from FlashcardBlockDTO

--- src/main/java/com/notes/api/dto/FlashcardBlockDTO.java
+++ src/main/java/com/notes/api/dto/FlashcardBlockDTO.java
@@ -4,19 +4,10 @@ import java.util.List;
 
 public class FlashcardBlockDTO extends BlockDTO {
 
-    private long id;
     private List<FlashcardDTO> data;
 
     public FlashcardBlockDTO() {
        setType(BlockType.FlashcardBlock);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public List<FlashcardDTO> getData() {
