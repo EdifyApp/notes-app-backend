b39b5aa7d12e2ddc9c05885ff288826afdf4d0d3
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-07-08 23:17:25 +0300

    Introduce service layer and sort blocks by location index

--- src/main/java/com/notes/api/controllers/NoteController.java
+++ src/main/java/com/notes/api/controllers/NoteController.java
@@ -4,50 +4,36 @@ import com.notes.api.controllers.responses.GetResponse;
 import com.notes.api.controllers.responses.NoteInfoListResponse;
 import com.notes.api.controllers.responses.SaveResponse;
 import com.notes.api.dto.NoteDTO;
-import com.notes.api.entities.Note;
-import com.notes.api.mappers.NoteDTOToNoteMapper;
-import com.notes.api.repositories.NoteRepository;
-import com.notes.api.responses.NoteInfo;
+import com.notes.api.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-import java.util.List;
 
 @RestController
 public class NoteController {
 
     @Autowired
-    NoteRepository noteRepository;
-
-    @Autowired
-    NoteDTOToNoteMapper mapper;
+    NoteService noteService;
 
     @GetMapping("/getnote")
     @ResponseBody
     public GetResponse getNoteById(@RequestParam long id) {
-        Note note = noteRepository.findById(id);
-        NoteDTO noteDTO = mapper.toNoteDTO(note);
-        if (note != null) {
+        NoteDTO noteDTO = noteService.getNoteById(id);
+        if (noteDTO != null) {
             return new GetResponse(noteDTO, "found note", true);
         }
-
         return new GetResponse(null, "could not find note with specified id", false);
     }
 
     @GetMapping("/getallnoteinfo")
     @ResponseBody
     public NoteInfoListResponse getAllNoteInfo() {
-        List<NoteInfo> noteInfoList = noteRepository.findAllBy();
-        return new NoteInfoListResponse(noteInfoList, true);
+        return new NoteInfoListResponse(noteService.getAllNoteInfo(), true);
     }
 
     @PostMapping("/savenote")
     @ResponseBody
     public SaveResponse createNote(@RequestBody NoteDTO noteDTO) {
-        noteDTO.setLastSaved(new Date());
-        Note note = mapper.toNote(noteDTO);
-        noteRepository.save(note);
-        return new SaveResponse(note.getId(), "Note successfully save", true);
+        long noteId = noteService.saveNote(noteDTO);
+        return new SaveResponse(noteId, "Note successfully save", true);
     }
 }
