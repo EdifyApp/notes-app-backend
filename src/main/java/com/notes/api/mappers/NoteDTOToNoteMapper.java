73247b2883f7514ca77683c649c825033cdd9f01
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-07-09 12:33:53 +0300

    Remove redundant mapper code

--- src/main/java/com/notes/api/mappers/NoteDTOToNoteMapper.java
+++ src/main/java/com/notes/api/mappers/NoteDTOToNoteMapper.java
@@ -1,6 +1,7 @@
 package com.notes.api.mappers;
 
-import com.notes.api.dto.*;
+import com.notes.api.dto.BlockDTO;
+import com.notes.api.dto.NoteDTO;
 import com.notes.api.entities.CodeBlock;
 import com.notes.api.entities.FlashcardBlock;
 import com.notes.api.entities.Note;
@@ -9,10 +10,10 @@ import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = "spring",
@@ -22,6 +23,15 @@ import java.util.stream.Collectors;
         BlockDTOToFlashcardBlockMapper.class,
 })
 public abstract class NoteDTOToNoteMapper {
+
+    @Autowired
+    BlockDTOToRichTextBlockMapper richTextBlockMapper;
+
+    @Autowired
+    BlockDTOToCodeBlockMapper codeBlockMapper;
+
+    @Autowired
+    BlockDTOToFlashcardBlockMapper flashcardBlockMapper;
 
     @Mapping(source = "blocks", target = "richTextBlocks")
     @Mapping(source = "blocks", target = "codeBlocks")
@@ -44,60 +54,9 @@ public abstract class NoteDTOToNoteMapper {
         List<CodeBlock> codeBlocks = note.getCodeBlocks();
         List<FlashcardBlock> flashcardBlocks = note.getFlashcardBlocks();
 
-        List<BlockDTO> blockDTOs = toRichTextBlockDTOs(richTextBlocks);
-        blockDTOs.addAll(toCodeBlockDTOs(codeBlocks));
-        blockDTOs.addAll(toFlashcardBlockDTOs(flashcardBlocks));
+        List<BlockDTO> blockDTOs = richTextBlockMapper.toBlockDTOs(richTextBlocks);
+        blockDTOs.addAll(codeBlockMapper.toBlockDTOs(codeBlocks));
+        blockDTOs.addAll(flashcardBlockMapper.toFlashcardBlockDTOs(flashcardBlocks));
         noteDTO.setBlocks(blockDTOs);
-    }
-
-    private List<BlockDTO> toRichTextBlockDTOs(List<RichTextBlock> richTextBlocks) {
-        if (richTextBlocks == null || richTextBlocks.isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        return richTextBlocks.stream().map(richTextBlock -> {
-            RichTextBlockDTO blockDTO = new RichTextBlockDTO();
-            blockDTO.setData(richTextBlock.getData());
-            blockDTO.setId(richTextBlock.getId());
-            blockDTO.setLocationIndex(richTextBlock.getLocationIndex());
-            return blockDTO;
-        }).collect(Collectors.toList());
-    }
-
-    private List<BlockDTO> toCodeBlockDTOs(List<CodeBlock> codeBlocks) {
-        if (codeBlocks == null || codeBlocks.isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        return codeBlocks.stream().map(codeBlock -> {
-            CodeBlockDTO blockDTO = new CodeBlockDTO();
-            blockDTO.setData(codeBlock.getData());
-            blockDTO.setId(codeBlock.getId());
-            blockDTO.setLocationIndex(codeBlock.getLocationIndex());
-            return blockDTO;
-        }).collect(Collectors.toList());
-    }
-
-    private List<BlockDTO> toFlashcardBlockDTOs(List<FlashcardBlock> flashcardBlocks) {
-        if (flashcardBlocks == null || flashcardBlocks.isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        return flashcardBlocks.stream()
-                .filter(flashcardBlock -> flashcardBlock.getFlashcards() != null)
-                .map(flashcardBlock -> {
-                    FlashcardBlockDTO blockDTO = new FlashcardBlockDTO();
-                    blockDTO.setId(flashcardBlock.getId());
-                    blockDTO.setLocationIndex(flashcardBlock.getLocationIndex());
-                    blockDTO.setData(
-                            flashcardBlock.getFlashcards().stream().map(flashcard -> {
-                                FlashcardDTO flashcardDTO = new FlashcardDTO();
-                                flashcardDTO.setQuestion(flashcard.getQuestion());
-                                flashcardDTO.setAnswer(flashcard.getAnswer());
-                                return flashcardDTO;
-                            }).collect(Collectors.toList())
-                    );
-                    return blockDTO;
-                }).collect(Collectors.toList());
     }
 }
