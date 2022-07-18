c15802d91bb1b7f6a29a6fad3e3e6ecf693c9db8
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-07-18 10:11:41 +0300

    Add note name to flashcard info response

--- src/main/java/com/notes/api/responses/FlashcardInfo.java
+++ src/main/java/com/notes/api/responses/FlashcardInfo.java
@@ -4,6 +4,7 @@ public interface FlashcardInfo {
     long getId();
     long getFlashcardBlockId();
     long getNoteId();
+    String getNoteNoteName();
     String getQuestion();
     String getAnswer();
 }
