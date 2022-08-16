268950b88c0b6e01016bc1a3346e8cdfaa1c8f84
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-08-16 23:45:37 +0200

    Add User entity

---
+++ src/main/java/com/notes/api/entities/User.java
@@ -0,0 +1,56 @@
+package com.notes.api.entities;
+
+import com.notes.api.entities.note.Note;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Note> notes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+}
