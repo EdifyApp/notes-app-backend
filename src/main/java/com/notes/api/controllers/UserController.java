597b06d72cd9b2178348ef52ef8a7cd3afb1306d
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-08-16 23:52:16 +0200

    Add user controller, service and repository

---
+++ src/main/java/com/notes/api/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.notes.api.controllers;
+
+import com.notes.api.controllers.responses.SignOnResponse;
+import com.notes.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/signon")
+    @ResponseBody
+    public SignOnResponse signOn(@RequestBody String token) {
+        String userId = userService.signOnUser(token);
+
+        if (userId.isEmpty()) {
+            return new SignOnResponse(false, "something went wrong signing on", "");
+        }
+        return new SignOnResponse(true, "successfully signed on", userId);
+    }
+
+}
