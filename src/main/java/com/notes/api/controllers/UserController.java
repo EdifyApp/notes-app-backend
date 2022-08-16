2aac33ba6171483e371b22dab20291dd2b766806
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-08-16 23:56:33 +0200

    Add auth request class

--- src/main/java/com/notes/api/controllers/UserController.java
+++ src/main/java/com/notes/api/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.notes.api.controllers;
 
+import com.notes.api.controllers.requests.AuthRequest;
 import com.notes.api.controllers.responses.SignOnResponse;
 import com.notes.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,8 @@ public class UserController {
 
     @PostMapping("/signon")
     @ResponseBody
-    public SignOnResponse signOn(@RequestBody String token) {
-        String userId = userService.signOnUser(token);
+    public SignOnResponse signOn(@RequestBody AuthRequest request) {
+        String userId = userService.signOnUser(request.getToken());
 
         if (userId.isEmpty()) {
             return new SignOnResponse(false, "something went wrong signing on", "");
---
+++ src/main/java/com/notes/api/controllers/requests/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.notes.api.controllers.requests;
+
+public class AuthRequest {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
