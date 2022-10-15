630803e8d87c711743bad7174331d1f05fc714f5
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-10-15 16:15:57 +0200

    Add allow list of urls for which firebase auth should be skipped

--- src/main/java/com/notes/FirebaseFilter.java
+++ src/main/java/com/notes/FirebaseFilter.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -18,38 +19,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FirebaseFilter extends OncePerRequestFilter {
 
     private final static String TOKEN_HEADER = "Authorization";
+
+    private static final List<String> ALLOWED_URLS = Arrays.asList(
+            "/signon",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
 
     private static final Logger logger = LoggerFactory.getLogger(FirebaseFilter.class);
 
     @Autowired
     private UserService userService;
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authToken = request.getHeader(TOKEN_HEADER);
 
+        if (authToken == null || authToken.isEmpty()) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "no firebase token");
+            return;
+        }
+
         try {
             Authentication authentication = getAndValidateAuthentication(authToken);
             if (authentication.getPrincipal() == null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "user is not authorised");
                 return;
             }
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (Exception ex) {
             logger.info(ex.getMessage());
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             return;
         }
 
 
         filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return ALLOWED_URLS.stream().anyMatch(p -> new AntPathMatcher().match(p, path));
     }
 
     private Authentication getAndValidateAuthentication(String authToken) throws Exception {
