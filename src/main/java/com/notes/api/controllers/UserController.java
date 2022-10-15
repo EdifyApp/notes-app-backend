3620a2a73a66da33a704627b8c65c44440a0e44e
Rifad Lafir <rifadahmedlafir@gmail.com>
2022-10-15 16:21:17 +0200

    Add get signed on user function and create user functionality

--- src/main/java/com/notes/api/controllers/UserController.java
+++ src/main/java/com/notes/api/controllers/UserController.java
@@ -4,20 +4,20 @@ import com.notes.api.controllers.responses.SignOnResponse;
 import com.notes.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
     @Autowired
     UserService userService;
 
     // TODO: exclude this endpoint from auth
     @PostMapping("/signon")
     @ResponseBody
-    public SignOnResponse signOn() {
-        String userId = userService.signOnUser();
+    public SignOnResponse signOn(@RequestHeader("Authorization") String token) {
+        String userId = userService.signOnUser(token);
 
         if (userId.isEmpty()) {
             return new SignOnResponse(false, "something went wrong signing on", "");
